0ba5cb8cbfc328bbd7532bc676f1c40c5ba99285
DhonataF <dhonatafreitas@gmail.com>
2015-11-18 18:13:49 -0300

    Backap e banco

--- Gramaticando.sql
+++ Gramaticando.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.3
+-- version 4.1.4
 -- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tempo de Geração: 
--- Versão do Servidor: 5.5.28
--- Versão do PHP: 5.3.18
+-- Host: 127.0.0.1
+-- Generation Time: 18-Nov-2015 às 22:11
+-- Versão do servidor: 5.6.15-log
+-- PHP Version: 5.5.8
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,9 +17,9 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Banco de Dados: `gramaticando`
+-- Database: `gramaticando`
 --
-CREATE DATABASE `gramaticando` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `gramaticando` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `gramaticando`;
 
 -- --------------------------------------------------------
@@ -28,6 +28,7 @@ USE `gramaticando`;
 -- Estrutura da tabela `itens`
 --
 
+DROP TABLE IF EXISTS `itens`;
 CREATE TABLE IF NOT EXISTS `itens` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item1` varchar(255) NOT NULL,
@@ -44,6 +45,7 @@ CREATE TABLE IF NOT EXISTS `itens` (
 -- Estrutura da tabela `quests`
 --
 
+DROP TABLE IF EXISTS `quests`;
 CREATE TABLE IF NOT EXISTS `quests` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `quest` varchar(255) NOT NULL,
@@ -58,11 +60,13 @@ CREATE TABLE IF NOT EXISTS `quests` (
 -- Estrutura da tabela `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   `pontuacao` double NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nome` (`nome`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
