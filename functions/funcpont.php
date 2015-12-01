c2546a7e7f7535c4d96faa163f74fdaf238ca80f
DhonataF <dhonatafreitas@gmail.com>
2015-11-30 21:08:56 -0300

    Inicio pontos

---
+++ functions/funcpont.php
@@ -0,0 +1,19 @@
+﻿<?php
+	
+	session_start();
+	
+	require_once 'Classes/DAO/conexao.php';
+	
+	$user = Users::find($_SESSION['user']);
+	
+	function acerto(){
+		$user->pontuacao = $user->pontuacao + 10;
+		$user->save();
+	}
+	
+	function erro(){
+		$user->pontuacao = $user->pontuacao + (10/4);
+		$user->save();
+	}
+	
+?>
