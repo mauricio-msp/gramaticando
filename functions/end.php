8d32b68dce413ca29b11b8f8dbf9080d5bc506ab
DhonataF <dhonatafreitas@gmail.com>
2015-12-02 18:52:52 -0300

    mesclar e função de questoes

---
+++ functions/end.php
@@ -0,0 +1,11 @@
+﻿<?php
+	
+    session_start();
+	
+	if($_SESSION['cont'] == 0):
+		echo 1;
+	else:
+		echo 2;
+	endif;
+    
+?>
---
+++ pages/pgregras.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta content="IE=edge" http-equiv="X-UA-Compatible">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Desafio - Gramaticando</title>
+        <!--CSS Bootstrap 3.3.5 -->
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    </head>
+    <body>
+        
+    </body> 
+</html>
